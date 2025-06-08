017e7acb0e25a8580bf1b646657fd461691e1b5f
danialhaghmoradi <daniyal1384422@gmail.com>
2025-06-09 02:51:46 +0330

    chore: add required imports for category slider and Swiper setup

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,18 @@ import header from "./header/header";
 import heroSlider from "./hero-slider/hero-slider";
 import swiperConfig from "./hero-slider/swiperConfig";
 import menu from "./menu/Menu";
-import { productDiscount_col_1, productDiscount_col_2 } from "./products_discounts/products_discounts";
-
-
+import {
+  productDiscount_col_1,
+  productDiscount_col_2,
+} from "./products_discounts/products_discounts";
+import sliderCategories from "./slidercategories/sliderCategories";
+import { swiperConfigSliderCategories } from "./slidercategories/swiperconfig-slidercategories";
 heroSlider();
 header();
 menu();
 swiperConfig();
 countdown();
-productDiscount_col_1()
-productDiscount_col_2()
+productDiscount_col_1();
+productDiscount_col_2();
+swiperConfigSliderCategories();
+sliderCategories();
